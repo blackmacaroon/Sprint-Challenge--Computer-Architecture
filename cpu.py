fa08dbd3c4a597a5d54ed63a075366674baedd47
Kayla <6lackmacaroon@gmail.com>
2019-10-18 09:59:46 -0600

    extracting functions from projects run function

--- cpu.py
+++ cpu.py
@@ -34,11 +34,15 @@ class CPU:
             print(" %02X" % self.register[i], end='')
         print()
 
-    def ram_read(self, MAR):
-        return self.ram[MAR]
+    def ram_read(self, address):   # MAR = address in memory
+        return self.ram[address]
 
-    def ram_write(self, MDR, MAR):
-        self.ram[MAR] = MDR
+    def ram_write(self, data, address):   # MAR = address, MDR = data at address
+        self.ram[address] = data
+
+    # def handle_push(self, address): 
+
+    # def handle_pop(self, address):
 
     ############ Arithmetic Logic Unit  ###########################
     def alu(self, op, reg_a, reg_b):
@@ -59,3 +63,95 @@ class CPU:
                 self.flags == 0b00000001
         else:
             raise Exception("Unsupported ALU operation")
+
+    def run(self):
+        """Run the CPU."""
+        PRN = 0b01000111
+        HLT = 0b00000001
+        LDI = 0b10000010
+        ADD = 0b10100000
+        SUB = 0b10100001
+        MUL = 0b10100010
+        DIV = 0b10100011
+        PUSH = 0b01000101
+        POP = 0b01000110
+        CALL = 0b01010000
+        RET = 0b00010001
+        CMP = 0b10100111
+        JMP = 0b01010100
+        JEQ = 0b01010101
+        JNE = 0b01010110
+        running = True
+        while running:
+            IR = self.ram[self.PC]
+            operandA = self.ram_read(self.PC + 1)
+            operandB = self.ram_read(self.PC + 2)
+            if IR == LDI:
+                # operandA = self.ram_read(self.PC + 1)
+                # operandB = self.ram_read(self.PC + 2)
+                self.register[operandA] = operandB
+                self.PC += 3
+            elif IR == PRN:
+                print(self.register[operandA])
+                self.PC += 2
+            elif IR == HLT:
+                running = False
+                self.PC += 1
+            elif IR == ADD:
+                self.alu("ADD", operandA, operandB)
+                self.PC += 3
+            elif IR == SUB:
+                self.alu("SUB", operandA, operandB)
+                self.PC += 3
+            elif IR == MUL:
+                self.alu("MUL", operandA, operandB)
+                self.PC += 3
+            elif IR == DIV:
+                self.alu("DIV", operandA, operandB)
+                self.PC += 3
+            elif IR == CMP:
+                self.alu("CMP", operandA, operandB)
+                self.PC += 3
+            elif IR == POP:
+                # copy the value from the address pointed to by the SP, to the given register
+                self.register[operandA] = self.ram[self.SPL]
+                # increment pointer location
+                self.SPL += 1
+                self.PC += 2
+            elif IR == PUSH:
+                self.SPL -= 1
+                self.ram[self.SPL] = self.register[operandA]
+                self.PC += 2
+            elif IR == CALL:
+                self.register[self.SPL] -= 1
+                self.ram[self.SPL] = self.PC +2
+                self.PC = self.register[operandA]
+            elif IR == RET:
+                self.PC = self.ram[self.SPL]
+                self.register[self.SPL] += 1
+            elif IR == JMP:
+                # Jump to the address stored in the given register
+                # Set the `PC` to the address stored in the given register
+                self.PC = self.register[operandA]  # A is the address, not the pointer, send the counter to the given address
+            # flags 00000LGE
+            elif IR == JEQ:
+                # If `equal` flag is set (true)
+                if self.flags == 0b00000001:
+                    # jump to the address stored in the given register
+                    self.PC = self.register[operandA]
+                #otherwise, skip to the next instruction PC += 2
+                else:
+                    self.PC += 2
+                
+            elif IR == JNE:
+                # If `E` flag is clear (false, 0)
+                if self.flags == 0b00000000:
+                    #jump to the address stored in the given register
+                    self.PC = self.register[operandA]
+                # otherwise, skip to the next instruction PC += 2
+                else:
+                    self.PC += 2
+
+            else:
+                print(f"Wait, what? {IR}")
+                sys.exit(1)
